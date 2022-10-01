a8653dcb1788c3de7f12b61e5c6fc0df2bf8932d
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-01 13:09:12 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.querySelector('body');
+
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+}
