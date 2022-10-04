2ebca3c29df2f6b511fb56b96b51394c253c10a0
Katerina Dietkova <katrinkir1296@gmail.com>
2022-10-04 11:52:04 +0300

    Fixed task 5

--- js/task-05.js
+++ js/task-05.js
@@ -6,5 +6,9 @@ const refs = {
 refs.input.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-    refs.output.textContent = event.currentTarget.value;
+  if (refs.input.value === "") {
+    refs.output.textContent = "Anonymous";
+    return;
+  }
+  refs.output.textContent = event.currentTarget.value;
 }
